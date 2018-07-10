2909630035ead46954492100f41bbaa0fb3cc2e4
Alexandr Milashensky <milashensky@leadport.ninja>
2018-07-10 18:39:34 +0300

    index now fully api\vue

--- measor/views.py
+++ measor/views.py
@@ -15,7 +15,6 @@ class IndexView(AuthRequered, TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context['tasks'] = get_tasks()
         return context
 
 
--- static/js/runner-vue.js
+++ static/js/runner-vue.js
@@ -16,12 +16,6 @@ $().ready(function() {
         },
         created: function() {
             let vm = this;
-            vm.$on('build_now', function(slug){
-                let task = vm.tasks.find(x => x.slug == slug)
-                if (task){
-                    vm.tasks[vm.tasks.indexOf(task)].build_now = true
-                }
-            })
             vm.resource.get().then(function(response) {
                 this.tasks = response.body;
             },function () {});
@@ -40,59 +34,21 @@ $().ready(function() {
             clearInterval(this.interval)
             return true
         },
-        components: {
-            'run-now': {
-                props: ['slug'],
-                template: `
-                    <div>
-                        <i v-show="!task.build_now" class="fa fa-clock-o text-info" tooltip="Run now" v-on:click="run(slug)"></i>
-                        <i v-show="task.build_now" class="fa fa-clock-o text-warning" tooltip="Added to queue"></i>
-                    </div>
-                `,
-                computed: {
-                    task: function(){
-                        return this.$parent.tasks.find(x => x.slug == this.slug) || {}
-                    }
-                },
-                methods: {
-                    run: function(slug){
-                        let vm = this;
-                        vm.$parent.resource.save({slug: vm.slug.trim()}, data={action: 'build_now'}).then(function(response) {
-                            if (response.body && response.body.success){
-                                this.$emit('build_now', vm.slug)
-                            }
-                        });
-                    }
-                },
+        methods: {
+            ts2date: function(ts){
+                let date = new Date(ts * 1000)
+                return date.toLocaleDateString() + ' ' + date.toLocaleTimeString()
             },
-            'runner-status': {
-                props: ['slug'],
-                template: `
-                    <div>
-                        <i v-show="!task.running && !task.pause" class="fa fa-sun-o text-info" tooltip="Wait"></i>
-                        <i v-show="task.running" class="fa fa-certificate fa-spin text-success" tooltip="Running now"></i>
-                        <i v-show="!task.running && task.pause" class="fa fa-certificate text-muted" tooltip="Paused"></i>
-                    </div>
-                `,
-                computed: {
-                    task: function(){
-                        return this.$parent.tasks.find(x => x.slug == this.slug) || {}
+            run: function(slug){
+                let vm = this;
+                vm.resource.save({slug: slug}, data={action: 'build_now'}).then(function(response) {
+                    if (response.body && response.body.success){
+                        let task = vm.tasks.find(x => x.slug == slug)
+                        if (task){
+                            vm.tasks[vm.tasks.indexOf(task)].build_now = true
+                        }
                     }
-                },
-            },
-            'task-status': {
-                props: ['slug'],
-                template: `
-                    <div>
-                        <span v-show="task.last_status != undefined" :class="(task.last_status? 'text-success': 'text-danger')">{{(task.last_status? 'Success': 'Error')}}</span>
-                        <span v-show="task.last_status == undefined">-</span>
-                    </div>
-                `,
-                computed: {
-                    task: function(){
-                        return this.$parent.tasks.find(x => x.slug == this.slug) || {}
-                    }
-                },
+                });
             }
         }
     })
